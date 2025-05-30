0ec9b7729bee23e46907a973f42ff23c4fc1eede
Muhammad Umair <93200938+umair763@users.noreply.github.com>
2025-05-30 16:47:07 +0500

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+   base: '/', 
    plugins: [react()],
    build: {
-      outDir: 'dist', // this is default, but keep explicit for clarity
+      outDir: 'dist', 
+
    },
    server: {
       port: 5173,
-      historyApiFallback: true,
    },
 });
