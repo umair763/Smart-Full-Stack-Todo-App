15969f0a9952aeca278642826b034443f0a498cc
Muhammad Umair <93200938+umair763@users.noreply.github.com>
2025-05-30 16:08:12 +0500

    path correction

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
-import { ThemeProvider } from './app/context/ThemeContext';
+import { ThemeProvider } from './context/ThemeContext';
 import AppRoutes from './app/routes/routes';
-import NotificationToast from './app/components/NotificationToast';
-import EnhancedBackground from './app/components/EnhancedBackground';
-import './app/styles/background.css';
+import NotificationToast from './components/NotificationToast';
+import EnhancedBackground from './components/EnhancedBackground';
+import './styles/background.css';
 
 function App() {
    return (
