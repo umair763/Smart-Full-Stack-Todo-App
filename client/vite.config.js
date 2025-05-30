dec8f7c4e42e30e20fbbea75405453abf8548aee
Muhammad Umair <93200938+umair763@users.noreply.github.com>
2025-05-30 17:00:58 +0500

    issues fixes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
    plugins: [react()],
    build: {
       outDir: 'dist',
+      assetsDir: 'assets',
+      rollupOptions: {
+         output: {
+            manualChunks: undefined,
+         },
+      },
    },
    server: {
       port: 5173,
    },
+   resolve: {
+      alias: {
+         '@': path.resolve(__dirname, './src'),
+      },
+   },
+   base: './',
 });
