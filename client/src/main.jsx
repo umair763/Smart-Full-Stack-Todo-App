ba8d4e4b807d74c5f1d2c127fb2bf366a556b887
Muhammad Umair <93200938+umair763@users.noreply.github.com>
2025-06-01 12:18:20 +0500

    context settings

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App.jsx';
 import './index.css';
 import { AuthProvider } from './app/context/AuthContext';
@@ -8,21 +9,23 @@ import { ThemeProvider } from './app/context/ThemeContext';
 import { NotificationProvider } from './app/context/NotificationContext';
 import { SocketProvider } from './app/context/SocketContext';
 
-// Hardcoded Google Client ID
-const GOOGLE_CLIENT_ID = '1093100000000-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com';
+// Google Client ID
+const GOOGLE_CLIENT_ID = '726557724768-qplqm3h12oea644a7pqmnvf26umqssfr.apps.googleusercontent.com';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
    <React.StrictMode>
       <BrowserRouter>
-         <ThemeProvider>
-            <AuthProvider>
-               <NotificationProvider>
+         <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+            <ThemeProvider>
+               <AuthProvider>
                   <SocketProvider>
-                     <App />
+                     <NotificationProvider>
+                        <App />
+                     </NotificationProvider>
                   </SocketProvider>
-               </NotificationProvider>
-            </AuthProvider>
-         </ThemeProvider>
+               </AuthProvider>
+            </ThemeProvider>
+         </GoogleOAuthProvider>
       </BrowserRouter>
    </React.StrictMode>
 );
